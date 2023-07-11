2d7185b71c0d76cee04b26c11f20703dad34542e
Nikunj <ravalnikunj499@gmail.com>
2023-07-11 12:38:31 +0530

    updated files

--- edit.php
+++ edit.php
@@ -1,9 +1,10 @@
 <?php
-include 'connect.php';
+include '../connect.php';
 $name = "";
 $email = "";
 $mobile = "";
 $password = "";
+$userId = $_GET['id'];
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
@@ -11,13 +12,12 @@ if (isset($_POST['submit'])) {
     $mobile = $_POST['mobile'];
     $password = $_POST['password'];
 
-    $sql = "insert into `users`(name,email,mobile,password) values ('$name','$email','$mobile','$password')";
+    $updateSql = "update users set name = '{$name}', email = '{$email}', mobile = '{$mobile}', password = '{$password}' where id = 6";    
+    $updateResult = mysqli_query($con, $updateSql);
 
-    $result = mysqli_query($con, $sql);
-
-    if ($result) {
+    if ($updateResult) {
         echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Success!</strong>
-        Your Entry succesfully!
+        Your Entry Updated succesfully!
          <button type="button" class="close" dta-dismiss="alert" arial-label="close">
          <span arial-hidden="true">&time</span>
          </button>
@@ -25,11 +25,20 @@ if (isset($_POST['submit'])) {
     } else {
         die(mysqli_error($con));
     }
-} else {
-    $row = 
-    $sql = "SELECT id, name, email, mobile FROM `users` where id = ";
-    $result = mysqli_query($con, $sql);
 }
+
+if(isset($userId) && !empty($userId)) {
+    $sql = "SELECT * FROM `users` where id = {$userId} LIMIT 1";
+    $result = mysqli_query($con, $sql);
+    $record = mysqli_fetch_object($result);
+    if(!$record) {
+        echo "Please go away!"; exit;
+    }
+    // print_r($record); exit;
+} else {
+    echo 'Please go away!'; exit;
+}
+
 ?>
 
 
@@ -51,28 +60,29 @@ if (isset($_POST['submit'])) {
 <body>
     <section class="form-connection">
         <div class="container py-5">
+            <h1>Update User</h1>
             <form method="post" action="">
                 <div class="form-group">
                     <label for="exampleInputName1" class="form-label">Name</label>
-                    <input type="Text" name="name" class="form-control" placeholder="Enter Your Name" autocomplete="off">
+                    <input type="Text" name="name" class="form-control" placeholder="Enter Your Name" value="<?php echo $record->name; ?>">
                 </div>
 
                 <div class="form-group">
                     <label for="exampleInputEmail1" class="form-label">Email</label>
-                    <input type="text" name="email" class="form-control" placeholder="Enter Your Email" autocomplete="off">
+                    <input type="text" name="email" class="form-control" placeholder="Enter Your Email" value="<?php echo $record->email; ?>">
                 </div>
 
                 <div class="form-group">
                     <label for="exampleInputMobile1" class="form-label">Mobile</label>
-                    <input type="text" name="mobile" class="form-control" placeholder="Enter Your Mobile-Number" autocomplete="off">
+                    <input type="text" name="mobile" class="form-control" placeholder="Enter Your Mobile-Number" value="<?php echo $record->mobile; ?>">
                 </div>
 
                 <div class="form-group">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control" placeholder="Enter Your Password" autocomplete="off">
+                    <input type="password" name="password" class="form-control" placeholder="Enter Your Password" value="<?php echo $record->password; ?>">
                 </div>
 
-                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                <button type="submit" class="btn btn-primary" name="submit" value="add_user">Submit</button>
             </form>
         </div>
     </section>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include 'connect.php';
+include '../connect.php';
 $name = "";
 $email = "";
 $mobile = "";
